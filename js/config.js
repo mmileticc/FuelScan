90a9a32715ec3e202371c27e44e9654201140cad
Milinko <milinko.miletic03@gmail.com>
2026-06-12 17:31:21 +0200

    Commit before hosting on github pages, ready for connection with render backend first demo version

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 export const SUPABASE_URL = "https://jybatqpvokssutompyto.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5YmF0cXB2b2tzc3V0b21weXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMTUwMjIsImV4cCI6MjA5NjU5MTAyMn0.W-zZN3dLJDn18m3qoL0EP8s4g2K32vFO7tyIWD-oN_Q";
-export const BACKEND_API_URL = "http://127.0.0.1:8000/parse-receipt";
+
+export const BACKEND_API_URL = "https://fuelscan-jjba.onrender.com/parse-receipt";
